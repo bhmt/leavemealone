c9a67318915651c9b0d58815a567ce982a775e65
roko@bebop <roko.mislov.m@gmail.com>
2024-12-26 21:17:13 +0100

    gh pages base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://bhmt.github.io/leavemealone/',
   server: {
     port: 8000,
   }
